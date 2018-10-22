79f2eb0985ceaaa4bf093d412b4338d604051e0d
gleydson <gleydson404@gmail.com>
2018-10-22 02:53:53 -0300

    Fix: fixes for sort some queries correctly

--- nasa_analysis_df.py
+++ nasa_analysis_df.py
@@ -30,7 +30,7 @@ def main(log_file):
 
     data_frames['unique_hosts'] = splited_data_frame.groupBy('host').count().filter('count = 1').select('host')
 
-    data_frames['top_20_request'] = splited_data_frame.groupBy('request').count().sort(desc( "count")).limit(5)
+    data_frames['top_20_request'] = splited_data_frame.groupBy('request').count().sort(desc("count")).limit(20)
 
     data_frames['total_http_404'] = splited_data_frame.groupBy('http_status').count().filter('http_status = "404"')
 
@@ -38,11 +38,10 @@ def main(log_file):
 
     data_frames['top_5_hosts_http_404'] = splited_data_frame.filter('http_status = "404"').groupBy('request').count().sort(col("count").desc()).limit(5)
 
-    data_frames['qty_http_404_per_day'] = splited_data_frame.filter('http_status = "404"').groupBy(splited_data_frame.timestamp.substr(1, 11).alias('day')).count()
-
+    data_frames['qty_http_404_per_day'] = splited_data_frame.filter('http_status = "404"').groupBy(splited_data_frame.timestamp.substr(1, 11).alias('day')).count().sort(desc('day'))
     data_frames['sum_bytes'] = splited_data_frame.select('content_size_in_bytes').groupBy().sum()
 
-    data_frames['bytes_per_day'] = splited_data_frame.select('content_size_in_bytes', 'timestamp').groupBy(splited_data_frame.timestamp.substr(1, 11).alias('day')).sum()
+    data_frames['bytes_per_day'] = splited_data_frame.select('content_size_in_bytes', 'timestamp').groupBy(splited_data_frame.timestamp.substr(1, 11).alias('day')).sum().sort('day')
 
     utils.export_all_queries_to_csv(data_frames)
 
